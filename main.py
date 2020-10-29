dd5ab4f205279159ef0aa95683dc9f124874d4b2
maggie3000 <72889935+maggie3000@users.noreply.github.com>
2020-10-29 13:14:53 -0700

    Fixed ip address

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def home():
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='4000', host='0.0.0.0')
+    app.run(debug=True, port='4000', host='127.0.0.1')
